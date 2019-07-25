d234894d9216590fd853cc5bee4acd9c5d5898ef
MouriNaruto <Mouri_Naruto@outlook.com>
2019-07-25 17:43:51 +0800

    Create Visual Studio Solution.

---
+++ NCleaner2/NCleaner2.cpp
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   NCleaner2
+ * FILE:      NCleaner2.cpp
+ * PURPOSE:   Implementation for NCleaner2
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <cstdio>
+
+int main()
+{
+    std::printf("Hello World!\n");
+
+	return 0;
+}
