9c2f9ec556387bcf041c21e724e305a6ec0ac4d5
Naruto Mouri <Mouri_Naruto@outlook.com>
2019-09-01 23:37:37 +0800

    Add NitCore.

---
+++ Nit/NitCore.cpp
@@ -0,0 +1,9 @@
+﻿/*
+ * PROJECT:   Nit - A Windows Image Tweaker based on DismCore
+ * FILE:      NitCore.cpp
+ * PURPOSE:   Implementation for Nit Core Library
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
---
+++ Nit/NitCore.h
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   Nit - A Windows Image Tweaker based on DismCore
+ * FILE:      NitCore.h
+ * PURPOSE:   Definition for Nit Core Library
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#ifndef NIT_CORE
+#define NIT_CORE
+
+#include <Windows.h>
+
+
+
+#endif
