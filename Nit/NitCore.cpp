52d1ef32288489cc1a21e3f05fc1547d292df7ef
MouriNaruto <Mouri_Naruto@outlook.com>
2019-09-03 08:35:57 +0800

    Add several functions for operating the Windows Overlay Filter to the Nit Core Library.

--- Nit/NitCore.cpp
+++ Nit/NitCore.cpp
@@ -7,3 +7,117 @@
  *
  * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
  */
+
+#include "NitCore.h"
+
+namespace Nit
+{
+    /**
+     * The information about the Windows Overlay Filter file provider.
+     */
+    typedef struct _WOF_FILE_PROVIDER_EXTERNAL_INFO
+    {
+        WOF_EXTERNAL_INFO Wof;
+        FILE_PROVIDER_EXTERNAL_INFO FileProvider;
+    } WOF_FILE_PROVIDER_EXTERNAL_INFO, *PWOF_FILE_PROVIDER_EXTERNAL_INFO;
+
+    /**
+     * @remark You can read the definition for this function in "NitCore.h".
+     */
+    DWORD WofRemoveFileCompressionAttribute(
+        _In_ HANDLE FileHandle)
+    {
+        if (::DeviceIoControl(
+            FileHandle,
+            FSCTL_DELETE_EXTERNAL_BACKING,
+            nullptr,
+            0,
+            nullptr,
+            0,
+            nullptr,
+            nullptr))
+        {
+            return ERROR_SUCCESS;
+        }
+        else
+        {
+            return ::GetLastError();
+        }
+    }
+
+    /**
+     * @remark You can read the definition for this function in "NitCore.h".
+     */
+    DWORD WofSetFileCompressionAttribute(
+        _In_ HANDLE FileHandle,
+        _In_ DWORD CompressionAlgorithm)
+    {
+        switch (CompressionAlgorithm)
+        {
+        case FILE_PROVIDER_COMPRESSION_XPRESS4K:
+        case FILE_PROVIDER_COMPRESSION_LZX:
+        case FILE_PROVIDER_COMPRESSION_XPRESS8K:
+        case FILE_PROVIDER_COMPRESSION_XPRESS16K:
+            break;
+        default:
+            return ERROR_INVALID_PARAMETER;
+        }
+
+        WOF_FILE_PROVIDER_EXTERNAL_INFO WofInfo = { 0 };
+
+        WofInfo.Wof.Version = WOF_CURRENT_VERSION;
+        WofInfo.Wof.Provider = WOF_PROVIDER_FILE;
+
+        WofInfo.FileProvider.Version = FILE_PROVIDER_CURRENT_VERSION;
+        WofInfo.FileProvider.Flags = 0;
+        WofInfo.FileProvider.Algorithm = CompressionAlgorithm;
+
+        if (::DeviceIoControl(
+            FileHandle,
+            FSCTL_SET_EXTERNAL_BACKING,
+            &WofInfo,
+            sizeof(WofInfo),
+            nullptr,
+            0,
+            nullptr,
+            nullptr))
+        {
+            return ERROR_SUCCESS;
+        }
+        else
+        {
+            return ::GetLastError();
+        }
+    }
+
+    /**
+     * @remark You can read the definition for this function in "NitCore.h".
+     */
+    DWORD WofGetFileCompressionAttribute(
+        _Out_ PDWORD CompressionAlgorithm,
+        _In_ HANDLE FileHandle)
+    {
+        if (!CompressionAlgorithm)
+            return ERROR_INVALID_PARAMETER;
+
+        WOF_FILE_PROVIDER_EXTERNAL_INFO WofInfo = { 0 };
+
+        if (::DeviceIoControl(
+            FileHandle,
+            FSCTL_GET_EXTERNAL_BACKING,
+            nullptr,
+            0,
+            &WofInfo,
+            sizeof(WofInfo),
+            nullptr,
+            nullptr))
+        {
+            *CompressionAlgorithm = WofInfo.FileProvider.Algorithm;
+            return ERROR_SUCCESS;
+        }
+        else
+        {
+            return ::GetLastError();
+        }
+    }
+}
--- Nit/NitCore.h
+++ Nit/NitCore.h
@@ -11,8 +11,118 @@
 #ifndef NIT_CORE
 #define NIT_CORE
 
+#ifndef __cplusplus
+#error "[NitCore] You should use a C++ compiler."
+#endif
+
 #include <Windows.h>
 
+namespace Nit
+{
+    /**
+     * Removes the Windows Overlay Filter file compression attribute.
+     *
+     * @param FileHandle A handle to the file on which the operation is to be
+     *        performed.
+     * @return Standard Win32 Error. If the function succeeds, the return value is
+     *         ERROR_SUCCESS.
+     * @remark The way to get a file handle for this operation:
+     *         HANDLE FileHandle = CreateFileW(
+     *             FileName.c_str(),
+     *             FILE_LIST_DIRECTORY | FILE_READ_ATTRIBUTES,
+     *             FILE_SHARE_READ | FILE_SHARE_DELETE,
+     *             nullptr,
+     *             OPEN_EXISTING,
+     *             FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_SEQUENTIAL_SCAN,
+     *             nullptr);
+     */
+    DWORD WofRemoveFileCompressionAttribute(
+        _In_ HANDLE FileHandle);
 
+    /**
+     * Sets the Windows Overlay Filter file compression attribute.
+     *
+     * @param FileHandle A handle to the file on which the operation is to be
+     *        performed.
+     * @param CompressionAlgorithm Specifies the compression algorithm that is
+     *        used to compress this file. Currently defined algorithms are:
+     *        FILE_PROVIDER_COMPRESSION_XPRESS4K: Indicates that the data for
+     *            the file should be compressed in 4kb chunks with the XPress
+     *            algorithm. This algorithm is designed to be computationally
+     *            lightweight, and provides for rapid access to data.
+     *        FILE_PROVIDER_COMPRESSION_LZX: Indicates that the data for the
+     *            file should be compressed in 32kb chunks with the LZX
+     *            algorithm. This algorithm is designed to be highly compact,
+     *            and provides for small footprint for infrequently accessed
+     *            data.
+     *        FILE_PROVIDER_COMPRESSION_XPRESS8K: Indicates that the data for
+     *            the file should be compressed in 8kb chunks with the XPress
+     *            algorithm.
+     *        FILE_PROVIDER_COMPRESSION_XPRESS16K: Indicates that the data for
+     *            the file should be compressed in 16kb chunks with the XPress
+     *            algorithm.
+     * @return Standard Win32 Error. If the function succeeds, the return value is
+     *         ERROR_SUCCESS.
+     * @remark The way to get a file handle for this operation:
+     *         HANDLE FileHandle = CreateFileW(
+     *             FileName.c_str(),
+     *             FILE_LIST_DIRECTORY | FILE_READ_ATTRIBUTES,
+     *             FILE_SHARE_READ | FILE_SHARE_DELETE,
+     *             nullptr,
+     *             OPEN_EXISTING,
+     *             FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_SEQUENTIAL_SCAN,
+     *             nullptr);
+     */
+    DWORD WofSetFileCompressionAttribute(
+        _In_ HANDLE FileHandle,
+        _In_ DWORD CompressionAlgorithm);
+
+    /**
+     * Gets the Windows Overlay Filter file compression attribute.
+     *
+     * @param CompressionAlgorithm A pointer to the compression algorithm that
+     *        is used to compress this file. Currently defined algorithms are:
+     *        FILE_PROVIDER_COMPRESSION_XPRESS4K: Indicates that the data for
+     *            the file should be compressed in 4kb chunks with the XPress
+     *            algorithm. This algorithm is designed to be computationally
+     *            lightweight, and provides for rapid access to data.
+     *        FILE_PROVIDER_COMPRESSION_LZX: Indicates that the data for the
+     *            file should be compressed in 32kb chunks with the LZX
+     *            algorithm. This algorithm is designed to be highly compact,
+     *            and provides for small footprint for infrequently accessed
+     *            data.
+     *        FILE_PROVIDER_COMPRESSION_XPRESS8K: Indicates that the data for
+     *            the file should be compressed in 8kb chunks with the XPress
+     *            algorithm.
+     *        FILE_PROVIDER_COMPRESSION_XPRESS16K: Indicates that the data for
+     *            the file should be compressed in 16kb chunks with the XPress
+     *            algorithm.
+     * @param FileHandle A handle to the file on which the operation is to be
+     *        performed.
+     * @return Standard Win32 Error. If the function succeeds, the return value is
+     *         ERROR_SUCCESS.
+     * @remark The way to get a file handle for this operation:
+     *         HANDLE FileHandle = CreateFileW(
+     *             FileName.c_str(),
+     *             FILE_LIST_DIRECTORY | FILE_READ_ATTRIBUTES,
+     *             FILE_SHARE_READ | FILE_SHARE_DELETE,
+     *             nullptr,
+     *             OPEN_EXISTING,
+     *             FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_SEQUENTIAL_SCAN,
+     *             nullptr);
+     */
+    DWORD WofGetFileCompressionAttribute(
+        _Out_ PDWORD CompressionAlgorithm,
+        _In_ HANDLE FileHandle);
+
+//
+//    HRESULT CompressFileWithNTFSCompression(HANDLE FileHandle)
+//    {
+//        //DeviceIoControl(FileHandle,FSCTL_SET_COMPRESSION,)
+//    }
+//
+//    HRESULT DecompressFileWithNTFSCompression();
+//
+}
 
 #endif
